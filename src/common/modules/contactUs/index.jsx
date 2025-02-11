124630a721947f3a5324f6fef5524423f1da691f
Prem Kavathiya <premkavathiya35@gmail.com>
2025-02-12 00:32:07 +0530

    only one useState stor

--- src/common/modules/contactUs/index.jsx
+++ src/common/modules/contactUs/index.jsx
@@ -2,25 +2,31 @@ import React, { useState } from 'react'
 import Button from '../../elements/button'
 
 function ContactUs() {
-    const [fname, setFname] = useState('')
-    const [lname, setLname] = useState('')
-    const handleSubmit = () => {
-        console.log(fname)
-        localStorage.setItem('fName', fname)
-        localStorage.setItem('lName', lname)
-        setFname('')
-        setLname('')
+    const [formData, setFormData] = useState({ fname: '', lname: '' })
+    
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setFormData(formData=> ({ ...formData, [name]: value }))
     }
+
+    const handleSubmit = () => {
+        console.log(formData.fname)
+        localStorage.setItem('fName', formData.fname)
+        localStorage.setItem('lName', formData.lname)
+        setFormData({ fname: '', lname: '' })
+    }
+
     return (
         <div>
             <label htmlFor="fname">First name:</label><br />
-            <input type="text" id="fname" name="fname" placeholder='please enter fname' value={fname} onChange={(event) => setFname(event.target.value)} /><br />
+            <input type="text" id="fname" name="fname" placeholder='please enter fname' value={formData.fname} onChange={handleChange} /><br />
             <label htmlFor="lname">Last name:</label><br />
-            <input type="text" id="lname" name="lname" placeholder='please enter lname' value={lname} onChange={(event) => setLname(event.target.value)} /><br /><br />
+            <input type="text" id="lname" name="lname" placeholder='please enter lname' value={formData.lname} onChange={handleChange} /><br /><br />
             <Button
                 buttonText={'Submit'}
                 handleOnClick={handleSubmit}
             />
+            
         </div>
     )
 }
