3e2c605beab5db063f2429f149bb6caf4ff0bb7e
Your Name <you@example.com>
2025-02-11 11:05:51 +0530

    feat: use effect

--- src/common/components/card/cardDetails/index.jsx
+++ src/common/components/card/cardDetails/index.jsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, useSearchParams } from 'react-router'
+import { Link, useParams, useSearchParams } from 'react-router'
 import { dataSet } from '../../../utils/static'
+import Button from '../../../elements/button'
 
 function CardDetails() {
     const [cardData, setCardData] = useState(JSON.parse(localStorage.getItem('data')))
+    const [isClicked, setIsClicked] = useState(false)
+    const [isClicked2, setIsClicked2] = useState(false)
+
     const params = useParams() // used to fetch details from dynamic routing
     const [searchParams, _] = useSearchParams()  // used to fetch query params from URL
     const title = searchParams.get('title') || ''
     const id = searchParams.get('id')
     console.log('filterdData**', dataSet.find(el => el.title === title))
     console.log('params**', params)
+
+    console.log('running on mounting out', isClicked);
+    console.log('running on mounting out 2', isClicked2);
+    useEffect(() => {
+        console.log('running on mounting in', isClicked);
+        console.log('running on mounting in 2', isClicked2);
+        return () => {
+            console.log('running on unmounting');
+
+        }
+    }, [isClicked])
 
     useEffect(() => {
         fetch("https://dummyjson.com/recipes")
@@ -23,6 +38,9 @@ function CardDetails() {
     return (
         <div>
             {title} this is card details {cardData?.title}
+            <Button handleOnClick={() => setIsClicked(!isClicked)} />
+            <Button handleOnClick={() => setIsClicked2(!isClicked2)} />
+            <Link to={'/'} > To Home</Link>
         </div>
     )
 }
