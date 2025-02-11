40c4d3497014959484854a057ff91a52d131ce54
Your Name <you@example.com>
2025-02-11 11:30:22 +0530

    feat: form handeling

--- src/common/modules/contactUs/index.jsx
+++ src/common/modules/contactUs/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../elements/button'
 
 function ContactUs() {
+    const [fname, setFname] = useState('')
+    const [lname, setLname] = useState('')
+    const handleSubmit = () => {
+        console.log(fname)
+        localStorage.setItem('fName', fname)
+        localStorage.setItem('lName', lname)
+        setFname('')
+        setLname('')
+    }
     return (
         <div>
-            this is contact us page
+            <label htmlFor="fname">First name:</label><br />
+            <input type="text" id="fname" name="fname" placeholder='please enter fname' value={fname} onChange={(event) => setFname(event.target.value)} /><br />
+            <label htmlFor="lname">Last name:</label><br />
+            <input type="text" id="lname" name="lname" placeholder='please enter lname' value={lname} onChange={(event) => setLname(event.target.value)} /><br /><br />
+            <Button
+                buttonText={'Submit'}
+                handleOnClick={handleSubmit}
+            />
         </div>
     )
 }
