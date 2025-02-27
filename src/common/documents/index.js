81aff8cde94a2fd95aa02af7137b9152b504d26a
Your Name <you@example.com>
2025-02-27 11:23:20 +0530

    chore: doc

--- src/common/documents/index.js
+++ src/common/documents/index.js
@@ -5,13 +5,20 @@
 // 3. Hooks should always be called at the top of the component
 // 4. Hooks should not be called inside any loop
 
+// useNavigate
+// useSearchParams
+// useId
+
 // Basic hooks
 // 1. useState
 // 2. useEffect
 // 3. useContext
 
-
 // 4. useRef
 // 5. useMemo - to cache values
 // 6. useCallback - to cache functions
 // 7. memo - to cache components
+
+// 8. useReducer
+// 9. useDispatch
+// 10. useSelector
