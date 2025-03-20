d491b4987745348a686167a0a0329def45a47987
Your Name <you@example.com>
2025-03-20 11:14:22 +0530

    feat: useReducer

--- src/common/modules/global/index.jsx
+++ src/common/modules/global/index.jsx
@@ -1,22 +1,37 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import SubComp1 from './subComp1'
 import SubComp2 from './subComp2'
+
+const localInitialState = {
+    count: 0,
+    text: 'test'
+}
+
+const localRed = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_TEXT':
+            return { ...state, text: action.payload }
+        case 'CHANGE_COUNT':
+            return { ...state, count: action.payload }
+        default:
+            return state
+    }
+}
 
 function Global() {
     const var0 = 'variable from global file'
     const [stateuplftn, setStateuplftn] = useState()
-    console.log('log from global', stateuplftn);
+    const [state, dispatch] = useReducer(localRed, localInitialState)
 
     return (
         <div>
             Global route
-            <SubComp1
-                var0={var0}
-                setStateuplftn={setStateuplftn}
-            />
-            <SubComp2 
-            data={stateuplftn}
-            />
+            <SubComp1 var0={var0} setStateuplftn={setStateuplftn} />
+            <SubComp2 data={stateuplftn} />
+            {state?.count}<br />
+            <button onClick={() => dispatch({ type: 'CHANGE_COUNT', payload: state.count + 1 })}>Increase Count</button><br />
+            {state?.text}<br />
+            <button onClick={() => dispatch({ type: 'CHANGE_TEXT', payload: state.text + ' test' })}>Change text</button>
         </div>
     )
 }
